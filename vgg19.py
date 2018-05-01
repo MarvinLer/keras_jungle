95c77b37aaf115f54f56b53912893f679d9e410d
Marvin Lerousseau <marvin.lerousseau@gmail.com>
2018-05-01 22:31:44 +0200

    initial commit

---
+++ vgg19.py
@@ -0,0 +1,64 @@
+from __future__ import print_function, division
+from keras.models import Model
+from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense
+# usage_example() function imports
+from keras.datasets import cifar10
+from keras.utils import to_categorical
+
+
+def vgg16(input_shape, number_classes):
+    input_image = Input(shape=input_shape)
+    x = Conv2D(64, (3, 3), strides=(1, 1), padding='same', activation='relu')(input_image)
+    x = Conv2D(64, (3, 3), strides=(1, 1), padding='same', activation='relu')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2))(x)
+
+    x = Conv2D(128, (3, 3), strides=(1, 1), padding='same', activation='relu')(x)
+    x = Conv2D(128, (3, 3), strides=(1, 1), padding='same', activation='relu')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2))(x)
+
+    x = Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu')(x)
+    x = Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu')(x)
+    x = Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu')(x)
+    x = Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2))(x)
+
+    x = Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu')(x)
+    x = Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu')(x)
+    x = Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu')(x)
+    x = Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2))(x)
+
+    x = Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu')(x)
+    x = Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu')(x)
+    x = Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu')(x)
+    x = Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2))(x)
+
+    x = Flatten()(x)
+    x = Dense(4096, activation='relu')(x)
+    x = Dense(4096, activation='relu')(x)
+    x = Dense(number_classes, activation='softmax')(x)
+
+    return Model(inputs=input_image, outputs=x)
+
+
+def usage_example():
+    image_shape = (32, 32, 3)
+    num_classes = 10
+    batch_size = 16
+    epochs = 10
+
+    # loads cifar10 + one-hot labels
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    y_train = to_categorical(y_train, num_classes)
+    y_test = to_categorical(y_test, num_classes)
+
+    model = vgg16(input_shape=image_shape, number_classes=num_classes)
+    model.compile(optimizer='SGD', loss='categorical_crossentropy')
+    model.summary()
+
+    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test), shuffle=True)
+
+
+if __name__ == '__main__':
+    usage_example()
