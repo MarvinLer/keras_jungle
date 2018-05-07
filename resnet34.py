428ff94e3bed1997e9535f74fa544d47f6668f0d
Marvin Lerousseau <marvin.lerousseau@gmail.com>
2018-05-07 07:28:02 +0200

    code refacto

--- resnet34.py
+++ resnet34.py
@@ -28,8 +28,8 @@ def resnet34(input_shape, number_classes):
 
     input_image = Input(shape=input_shape)
     # First block
-    x = Conv2D(64, (7, 7), strides=(2, 2), padding='same', activation='relu', name='conv1')(input_image)
-    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same', name='maxpool1')(x)
+    x = Conv2D(64, (7, 7), strides=(2, 2), padding='same', activation='relu')(input_image)
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
 
     # Second block
     x = building_block(x, n_filters=64)
@@ -70,10 +70,8 @@ def usage_example():
 
     # loads cifar10 + resize pixels into [0, 1] + one-hot labels
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    x_train = x_train.astype('float32')
-    x_test = x_test.astype('float32')
-    x_train /= 255
-    x_test /= 255
+    x_train = x_train.astype('float32') / 255.
+    x_test = x_test.astype('float32') / 255.
     y_train = to_categorical(y_train, num_classes)
     y_test = to_categorical(y_test, num_classes)
 
